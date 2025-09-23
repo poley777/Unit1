2fd100011fcf847810aec1f442980c82e110e951
pedro.poley <pedro.poley@106-21.info2>
2025-09-23 12:20:35 +0200

    Exercise1-5

--- src/unit1/Exercise05.java
+++ src/unit1/Exercise05.java
@@ -10,14 +10,20 @@ public class Exercise05 {
 
         // Variable for radius
         double radio;
+        
+        //Variable for length
+        double longitud;
+        
+        //Variable for area
+        double area;
 
         // We ask the user to enter the radius
         System.out.print("Introduzca el radio de la circunferencia (puede tener decimales): ");
         radio = reader.nextDouble();
 
         // We calculate the length and area
-        double longitud = 2 * Math.PI * radio;
-        double area = Math.PI * Math.pow(radio, 2);
+         longitud = 2 * Math.PI * radio;
+         area = Math.PI * Math.pow(radio, 2);
 
         // We show the results
         System.out.println("La longitud de la circunferencia es: " + longitud);
